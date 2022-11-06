e02650876cea7d5f056c1461565970692f9dac00
Nicamoyanof <87718668+Nicamoyanof@users.noreply.github.com>
2022-11-06 12:45:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -56,172 +56,179 @@ NOVIEMBRE_2022: {
 };
 
 $(document).ready(function () {
-  // month parsing
-  month_idx = "AGOSTO_2022";
-  $("#select_month").on("change", function () {
-    month_idx = document.getElementById("select_month").value;
-  });
-  
-  setTimeout(function () {
+    // month parsing
+    month_idx = "AGOSTO_2022";
+
+    console.log("payment_method");
+
+    setTimeout(function () {
         payment_method = document.getElementById("modalidad_de_pago").value;
+        month_idx = document.getElementById("select_month").value;
+        select_value_idx = document.getElementById("cama_adentro").value;
+        valSelectJob = document.getElementById("job_type").value;
+        patagonia_percentage = document.getElementById("patagonia").value;
         console.log(payment_method);
-    console.log("holas");
     }, 5000);
 
-  $("#job_type").on("change", function () {
-    valSelectJob = $(this).val();
-    select_value = 0;
-    if (valSelectJob == "cama_adentro") {
-      $("#retiro_mensual").hide();
-      $("#cama_adentro").show();
-      $("#label_categories").show();
-      $("#job_time").show();
-      $("#label_job_time").show();
-      $("#button_calculate").show();
-      $("#label_modalidad_de_pago").show();
-      $("#modalidad_de_pago").show();
-      $("#label_patagonia").show();
-      $("#patagonia").show();
-      $("#label_antiguedad").show();
-      $("#antiguedad").show();
-    } else if (valSelectJob == "retiro_mensual") {
-      $("#cama_adentro").hide();
-      $("#retiro_mensual").show();
-      $("#label_categories").show();
-      $("#job_time").show();
-      $("#label_job_time").show();
-      $("#button_calculate").show();
-      $("#label_modalidad_de_pago").show();
-      $("#modalidad_de_pago").show();
-      $("#label_patagonia").show();
-      $("#patagonia").show();
-      $("#label_antiguedad").show();
-      $("#antiguedad").show();
-    } else {
-      $("#cama_adentro").hide();
-      $("#retiro_mensual").hide();
-      $("#label_categories").hide();
-      $("#job_time").hide();
-      $("#label_job_time").hide();
-      $("#button_calculate").hide();
-      $("#label_modalidad_de_pago").hide();
-      $("#modalidad_de_pago").hide();
-      $("#label_patagonia").hide();
-      $("#patagonia").hide();
-      $("#label_antiguedad").hide();
-      $("#antiguedad").hide();
-    }
-  });
-  $("select").on("change", function () {
-    $("#job_time").val(0);
-    $("#antiguedad").val(0);
-    $("#basictextDiv").hide();
-    $("#antiguedadtextDiv").hide();
-    $("#patagoniatextDiv").hide();
-    $("#totaltextDiv").hide();
-    $("#errorDiv").hide();
-  });
-  $("#retiro_mensual").on("change", function () {
-    select_value_idx = document.getElementById("retiro_mensual").value;
-  });
-  $("#cama_adentro").on("change", function () {
-    select_value_idx = document.getElementById("cama_adentro").value;
-  });
-  $("#modalidad_de_pago").on("change", function () {
-    payment_method = document.getElementById("modalidad_de_pago").value;
-  });
-  $("#patagonia").on("change", function () {
-    patagonia_percentage = document.getElementById("patagonia").value;
-  });
-  $("#button_calculate").on("click", function () {
-    jobTime = $("#job_time").val();
-    antiguedad = $("#antiguedad").val();
-    var divB = document.getElementById("basictextDiv");
-    var divA = document.getElementById("antiguedadtextDiv");
-    var divP = document.getElementById("patagoniatextDiv");
-    var divT = document.getElementById("totaltextDiv");
-    var divE = document.getElementById("errorDiv");
-    $("#basictextDiv").hide();
-    $("#antiguedadtextDiv").hide();
-    $("#patagoniatextDiv").hide();
-    $("#totaltextDiv").hide();
-    if (payment_method === "pago_mensual") {
-      if (valSelectJob == "cama_adentro") {
-        select_value = CONSTANTS[month_idx].MENSUAL_CAMA_ADENTRO[select_value_idx];
-      } else if (valSelectJob == "retiro_mensual") {
-        select_value = CONSTANTS[month_idx].MENSUAL_RETIRO[select_value_idx];
-      }
-      if (jobTime >= 24) {
-        producto = select_value / 48;
-        basic = producto * jobTime;
-        patagonia = basic * patagonia_percentage;
-        antiguedad = basic * antiguedad * 0.01;
-        total = basic + antiguedad + patagonia;
-        basic = basic.toFixed(2);
-        patagonia = patagonia.toFixed(2);
-        antiguedad = antiguedad.toFixed(2);
-        total = total.toFixed(2);
-        divB.textContent = "-Pago basico por mes: $ ".concat(basic);
-        if (antiguedad > 0) {
-          divA.textContent = "-Extra antiguedad: $ ".concat(antiguedad);
-          $("#antiguedadtextDiv").show();
+    $("#select_month").on("change", function () {
+        month_idx = document.getElementById("select_month").value;
+    });
+
+
+    $("#job_type").on("change", function () {
+        valSelectJob = $(this).val();
+        select_value = 0;
+        if (valSelectJob == "cama_adentro") {
+            $("#retiro_mensual").hide();
+            $("#cama_adentro").show();
+            $("#label_categories").show();
+            $("#job_time").show();
+            $("#label_job_time").show();
+            $("#button_calculate").show();
+            $("#label_modalidad_de_pago").show();
+            $("#modalidad_de_pago").show();
+            $("#label_patagonia").show();
+            $("#patagonia").show();
+            $("#label_antiguedad").show();
+            $("#antiguedad").show();
+        } else if (valSelectJob == "retiro_mensual") {
+            $("#cama_adentro").hide();
+            $("#retiro_mensual").show();
+            $("#label_categories").show();
+            $("#job_time").show();
+            $("#label_job_time").show();
+            $("#button_calculate").show();
+            $("#label_modalidad_de_pago").show();
+            $("#modalidad_de_pago").show();
+            $("#label_patagonia").show();
+            $("#patagonia").show();
+            $("#label_antiguedad").show();
+            $("#antiguedad").show();
+        } else {
+            $("#cama_adentro").hide();
+            $("#retiro_mensual").hide();
+            $("#label_categories").hide();
+            $("#job_time").hide();
+            $("#label_job_time").hide();
+            $("#button_calculate").hide();
+            $("#label_modalidad_de_pago").hide();
+            $("#modalidad_de_pago").hide();
+            $("#label_patagonia").hide();
+            $("#patagonia").hide();
+            $("#label_antiguedad").hide();
+            $("#antiguedad").hide();
         }
-        if (patagonia > 0) {
-          divP.textContent = "-Adicional Patagonia: $ ".concat(patagonia);
-          $("#patagoniatextDiv").show();
-        }
-        if (patagonia > 0 || antiguedad > 0) {
-          $("#basictextDiv").show();
-        }
-        divT.textContent = "Pago total por mes: $ ".concat(total);
-        $("#totaltextDiv").show();
-        $("#errorDiv").hide();
-      } else {
-        divE.textContent = "Por menos de 24 horas semanales, corresponde pago por hora.";
-        $("#errorDiv").show();
+    });
+    $("select").on("change", function () {
+        $("#job_time").val(0);
+        $("#antiguedad").val(0);
         $("#basictextDiv").hide();
         $("#antiguedadtextDiv").hide();
         $("#patagoniatextDiv").hide();
         $("#totaltextDiv").hide();
-      }
-    } else if (payment_method === "pago_por_hora") {
-      if (valSelectJob == "cama_adentro") {
-        select_value = CONSTANTS[month_idx].POR_HORA_CAMA_ADENTRO[select_value_idx];
-      } else if (valSelectJob == "retiro_mensual") {
-        select_value = CONSTANTS[month_idx].POR_HORA_RETIRO[select_value_idx];
-      }
-      if (jobTime < 24) {
-        basic = select_value * jobTime;
-        patagonia = basic * patagonia_percentage;
-        antiguedad = basic * antiguedad * 0.01;
-        total = basic + antiguedad + patagonia;
-        basic = basic.toFixed(2);
-        patagonia = patagonia.toFixed(2);
-        antiguedad = antiguedad.toFixed(2);
-        total = total.toFixed(2);
-        divB.textContent = "-Pago basico por semana: $ ".concat(basic);
-        if (antiguedad > 0) {
-          divA.textContent = "-Antiguedad: $ ".concat(antiguedad);
-          $("#antiguedadtextDiv").show();
-        }
-        if (patagonia > 0) {
-          divP.textContent = "-Adicional Patagonia: $ ".concat(patagonia);
-          $("#patagoniatextDiv").show();
-        }
-        if (patagonia > 0 || antiguedad > 0) {
-          $("#basictextDiv").show();
-        }
-        divT.textContent = "Pago total por semana: $ ".concat(total);
-        $("#totaltextDiv").show();
         $("#errorDiv").hide();
-      } else {
-        divE.textContent = "Desde 24 horas semanales, corresponde pago mensual.";
-        $("#errorDiv").show();
+    });
+    $("#retiro_mensual").on("change", function () {
+        select_value_idx = document.getElementById("retiro_mensual").value;
+    });
+    $("#cama_adentro").on("change", function () {
+        select_value_idx = document.getElementById("cama_adentro").value;
+    });
+    $("#modalidad_de_pago").on("change", function () {
+        payment_method = document.getElementById("modalidad_de_pago").value;
+    });
+    $("#patagonia").on("change", function () {
+        patagonia_percentage = document.getElementById("patagonia").value;
+    });
+    $("#button_calculate").on("click", function () {
+        jobTime = $("#job_time").val();
+        antiguedad = $("#antiguedad").val();
+        var divB = document.getElementById("basictextDiv");
+        var divA = document.getElementById("antiguedadtextDiv");
+        var divP = document.getElementById("patagoniatextDiv");
+        var divT = document.getElementById("totaltextDiv");
+        var divE = document.getElementById("errorDiv");
         $("#basictextDiv").hide();
         $("#antiguedadtextDiv").hide();
         $("#patagoniatextDiv").hide();
         $("#totaltextDiv").hide();
-      }
-    }
-  });
-});
+        if (payment_method === "pago_mensual") {
+            if (valSelectJob == "cama_adentro") {
+                select_value = CONSTANTS[month_idx].MENSUAL_CAMA_ADENTRO[select_value_idx];
+            } else if (valSelectJob == "retiro_mensual") {
+                select_value = CONSTANTS[month_idx].MENSUAL_RETIRO[select_value_idx];
+            }
+            if (jobTime >= 24) {
+                producto = select_value / 48;
+                basic = producto * jobTime;
+                patagonia = basic * patagonia_percentage;
+                antiguedad = basic * antiguedad * 0.01;
+                total = basic + antiguedad + patagonia;
+                basic = basic.toFixed(2);
+                patagonia = patagonia.toFixed(2);
+                antiguedad = antiguedad.toFixed(2);
+                total = total.toFixed(2);
+                divB.textContent = "-Pago basico por mes: $ ".concat(basic);
+                if (antiguedad > 0) {
+                    divA.textContent = "-Extra antiguedad: $ ".concat(antiguedad);
+                    $("#antiguedadtextDiv").show();
+                }
+                if (patagonia > 0) {
+                    divP.textContent = "-Adicional Patagonia: $ ".concat(patagonia);
+                    $("#patagoniatextDiv").show();
+                }
+                if (patagonia > 0 || antiguedad > 0) {
+                    $("#basictextDiv").show();
+                }
+                divT.textContent = "Pago total por mes: $ ".concat(total);
+                $("#totaltextDiv").show();
+                $("#errorDiv").hide();
+            } else {
+                divE.textContent = "Por menos de 24 horas semanales, corresponde pago por hora.";
+                $("#errorDiv").show();
+                $("#basictextDiv").hide();
+                $("#antiguedadtextDiv").hide();
+                $("#patagoniatextDiv").hide();
+                $("#totaltextDiv").hide();
+            }
+        } else if (payment_method === "pago_por_hora") {
+            if (valSelectJob == "cama_adentro") {
+                select_value = CONSTANTS[month_idx].POR_HORA_CAMA_ADENTRO[select_value_idx];
+            } else if (valSelectJob == "retiro_mensual") {
+                select_value = CONSTANTS[month_idx].POR_HORA_RETIRO[select_value_idx];
+            }
+            if (jobTime < 24) {
+                basic = select_value * jobTime;
+                patagonia = basic * patagonia_percentage;
+                antiguedad = basic * antiguedad * 0.01;
+                total = basic + antiguedad + patagonia;
+                basic = basic.toFixed(2);
+                patagonia = patagonia.toFixed(2);
+                antiguedad = antiguedad.toFixed(2);
+                total = total.toFixed(2);
+                divB.textContent = "-Pago basico por semana: $ ".concat(basic);
+                if (antiguedad > 0) {
+                    divA.textContent = "-Antiguedad: $ ".concat(antiguedad);
+                    $("#antiguedadtextDiv").show();
+                }
+                if (patagonia > 0) {
+                    divP.textContent = "-Adicional Patagonia: $ ".concat(patagonia);
+                    $("#patagoniatextDiv").show();
+                }
+                if (patagonia > 0 || antiguedad > 0) {
+                    $("#basictextDiv").show();
+                }
+                divT.textContent = "Pago total por semana: $ ".concat(total);
+                $("#totaltextDiv").show();
+                $("#errorDiv").hide();
+            } else {
+                divE.textContent = "Desde 24 horas semanales, corresponde pago mensual.";
+                $("#errorDiv").show();
+                $("#basictextDiv").hide();
+                $("#antiguedadtextDiv").hide();
+                $("#patagoniatextDiv").hide();
+                $("#totaltextDiv").hide();
+            }
+        }
+    })
+})
